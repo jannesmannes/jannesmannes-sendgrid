2f795a470317f371209f2c93eb73bd1b09131936
Jorn Henkes <jorn@jannesmannes.nl>
2021-10-26 10:27:51 +0200

    Composer update

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'type' => 'wordpress-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '3ec9c2f9e7d265773a978560044cb839cd22de84',
+        'name' => 'jannesenmannes/sendgrid-email-delivery-simplified',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'jannesenmannes/sendgrid-email-delivery-simplified' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'type' => 'wordpress-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '3ec9c2f9e7d265773a978560044cb839cd22de84',
+            'dev_requirement' => false,
+        ),
+    ),
+);
